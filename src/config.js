6297071316e5492cbfafad58b9a6c506d6c87a69
Sabith Pocker <sabithpocker@gmail.com>
2022-09-12 16:15:18 +0530

    gatsby builder test

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// using module.exports here so gatsby-config can read it
+module.exports = {
+  // TODO: Replace next line with your own public API key
+  builderAPIKey: 'ea30816b43c240beb4018b8e41acfeb6'
+};
